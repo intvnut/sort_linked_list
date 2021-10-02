eac2610fa6846e497db4de0f4a2ee8d94789cb40
Joe Zbiciak <intvnut@gmail.com>
2021-10-01 21:12:17 -0500

    Rework benchmark_int64 to use sort_registry
    The Int64List benchmark now uses the sort registry to drive the benchmark.
    It also uses common code to perform the warmup and the main test.

--- benchmark_int64.c
+++ benchmark_int64.c
@@ -9,23 +9,28 @@
 #include <stdlib.h>
 #include <time.h>
 
-#include "bui1_merge_sort.h"
-#include "bui2_merge_sort.h"
 #include "list_node.h"
 #include "list_sort.h"
 #include "list_types.h"
 #include "mt64.h"
-#include "tdi1_merge_sort.h"
-#include "tdi2_merge_sort.h"
-#include "tdr1_merge_sort.h"
-#include "tdr2_merge_sort.h"
-#include "tdq1_quick_sort.h"
 
 // Returns the current time in seconds.
 static double now(void) {
   struct timespec ts;
   clock_gettime(CLOCK_MONOTONIC, &ts);
   return ts.tv_sec + 1e-9 * ts.tv_nsec;
+}
+
+// Prints the set of sort names as column headings for a CSV.  The context
+// argument sets the label for the first column, to allow us to distinguish the
+// warmup pass from the main benchmark.
+static void print_csv_header(const char *context) {
+  fputs(context, stdout);
+  for (size_t i = 0; i < sort_registry.length; ++i) {
+    printf(",%s", sort_registry.entry[i].name);
+  }
+  putchar('\n');
+  fflush(stdout);
 }
 
 // Creates a randomized linked list of int64_t in the designated buffer, with
@@ -122,6 +127,48 @@ static TestResult run_int64_list_test(void *const buf, const size_t elems,
   return test_result;
 }
 
+static void run_int64_list_tests(
+    void *const buf, TestResult *const tr, double *const tot_time,
+    const size_t elems, const int seed_lo, const int seed_hi) {
+  printf("%zu", elems); fflush(stdout);
+
+  for (size_t i = 0; i < sort_registry.length; ++i) {
+    tot_time[i] = 0.;
+  }
+
+  for (int seed = seed_lo; seed <= seed_hi; ++seed) {
+    for (size_t i = 0; i < sort_registry.length; ++i) {
+      tr[i] = run_int64_list_test(buf, elems,
+                                  sort_registry.entry[i].fxn, seed);
+      tot_time[i] += tr[i].time;
+    }
+
+    // Now check that they all return the same checksum.
+    bool ok = true;
+    for (size_t i = 1; i < sort_registry.length; ++i) {
+      if (tr[0].csum != tr[i].csum) {
+        ok = false;
+      }
+    }
+
+    if (!ok) {
+      printf("\nFAIL");
+      for (size_t i = 0; i < sort_registry.length; ++i) {
+        printf(",%" PRIX64, tr[i].csum);
+      }
+      putchar('\n');
+      exit(1);
+    }
+  }
+
+  double seed_scale = 1.0 / (seed_hi - seed_lo + 1);
+  for (size_t i = 0; i < sort_registry.length; ++i) {
+    printf(",%g", tot_time[i] * seed_scale);
+  }
+  putchar('\n');
+  fflush(stdout);
+}
+
 // Currently, 256MiB.
 #define MAX_POW2  (28)
 #define MAX_BYTES (1ull << MAX_POW2)
@@ -129,48 +176,20 @@ static TestResult run_int64_list_test(void *const buf, const size_t elems,
 
 int main() {
   void *buf = malloc(MAX_BYTES);
+  TestResult *tr = calloc(sizeof(TestResult), sort_registry.length);
+  double *tot_time = calloc(sizeof(double), sort_registry.length);
 
-  // Warmup.
-  {
-    const size_t elems = MAX_BYTES / sizeof(Int64ListNode);
-    printf("Warmup,Bottom-Up Iterative 1,Bottom-Up Iterative 2,"
-           "Top-Down Recursive 1,Top-Down Recursive 2,Top-Down Recursive 3,"
-           "Top-Down Iterative 1,Top-Down Iterative 2\n"
-           "%zu", elems);
-    fflush(stdout);
-
-    const TestResult bui1_tr = run_int64_list_test(buf, elems, bui1_merge_sort, 0);
-    printf(",%f", bui1_tr.time); fflush(stdout);
-    const TestResult bui2_tr = run_int64_list_test(buf, elems, bui2_merge_sort, 0);
-    printf(",%f", bui2_tr.time); fflush(stdout);
-    const TestResult tdr1_tr = run_int64_list_test(buf, elems, tdr1_merge_sort, 0);
-    printf(",%f", tdr1_tr.time); fflush(stdout);
-    const TestResult tdr2_tr = run_int64_list_test(buf, elems, tdr2_merge_sort, 0);
-    printf(",%f", tdr2_tr.time); fflush(stdout);
-    const TestResult tdq1_tr = run_int64_list_test(buf, elems, tdq1_quick_sort, 0);
-    printf(",%f", tdq1_tr.time); fflush(stdout);
-    const TestResult tdi1_tr = run_int64_list_test(buf, elems, tdi1_merge_sort, 0);
-    printf(",%f", tdi1_tr.time); fflush(stdout);
-    const TestResult tdi2_tr = run_int64_list_test(buf, elems, tdi2_merge_sort, 0);
-    printf(",%f", tdi2_tr.time); fflush(stdout);
-
-    if (bui1_tr.csum != bui2_tr.csum || bui1_tr.csum != tdr1_tr.csum ||
-        bui1_tr.csum != tdr2_tr.csum || bui1_tr.csum != tdq1_tr.csum ||
-        bui1_tr.csum != tdi1_tr.csum || bui1_tr.csum != tdi2_tr.csum) {
-      printf(",Warmup Failed: " "%" PRIX64 " %" PRIX64
-             " %" PRIX64 " %" PRIX64 " %" PRIX64 " %" PRIX64 " %" PRIX64 "\n",
-             bui1_tr.csum, bui2_tr.csum, tdr1_tr.csum, tdr2_tr.csum,
-             tdq1_tr.csum, tdi1_tr.csum, tdi2_tr.csum);
-      exit(1);
-    } else {
-      printf(",Warmup passed.\n");
-    }
+  if (!buf || !tr || !tot_time) {
+    fprintf(stderr, "Memory allocation failed.\n");
+    exit(1);
   }
 
-  printf("Elems,Bottom-Up Iterative 1,Bottom-Up Iterative 2,"
-         "Top-Down Recursive 1,Top-Down Recursive 2,Top-Down Recursive 3,"
-         "Top-Down Iterative 1,Top-Down Iterative 2\n");
-  fflush(stdout);
+  // Warmup.
+  const size_t elems = MAX_BYTES / sizeof(Int64ListNode);
+  print_csv_header("Warmup");
+  run_int64_list_tests(buf, tr, tot_time, elems, 0, 0);
+
+  print_csv_header("Elems");
 
   // Now run all three on different sizes up to MAX_BYTES, using eight sizes
   // between each power of 2, and NUM_SEEDS different seeds for each size.
@@ -187,59 +206,7 @@ int main() {
       }
       prev_elems = elems;
 
-      printf("%zu", elems); fflush(stdout);
-
-      double tot_bui1_time = 0., tot_bui2_time = 0.,
-             tot_tdr1_time = 0., tot_tdr2_time = 0., tot_tdq1_time = 0.,
-             tot_tdi1_time = 0., tot_tdi2_time = 0.;
-
-      for (int seed = 1; seed <= NUM_SEEDS; ++seed) {
-        const TestResult bui1_tr =
-          run_int64_list_test(buf, elems, bui1_merge_sort, seed);
-        const TestResult bui2_tr =
-          run_int64_list_test(buf, elems, bui2_merge_sort, seed);
-        const TestResult tdr1_tr =
-          run_int64_list_test(buf, elems, tdr1_merge_sort, seed);
-        const TestResult tdr2_tr =
-          run_int64_list_test(buf, elems, tdr2_merge_sort, seed);
-        const TestResult tdq1_tr =
-          run_int64_list_test(buf, elems, tdq1_quick_sort, seed);
-        const TestResult tdi1_tr =
-          run_int64_list_test(buf, elems, tdi1_merge_sort, seed);
-        const TestResult tdi2_tr =
-          run_int64_list_test(buf, elems, tdi2_merge_sort, seed);
-
-        tot_bui1_time += bui1_tr.time;
-        tot_bui2_time += bui2_tr.time;
-        tot_tdr1_time += tdr1_tr.time;
-        tot_tdr2_time += tdr2_tr.time;
-        tot_tdq1_time += tdq1_tr.time;
-        tot_tdi1_time += tdi1_tr.time;
-        tot_tdi2_time += tdi2_tr.time;
-
-        if (bui1_tr.csum != bui2_tr.csum || bui1_tr.csum != tdr1_tr.csum ||
-            bui1_tr.csum != tdr2_tr.csum || bui1_tr.csum != tdq1_tr.csum ||
-            bui1_tr.csum != tdi1_tr.csum || bui1_tr.csum != tdi2_tr.csum) {
-          printf(",Sort Failed: %" PRIX64 " %" PRIX64 " %" PRIX64 " %" PRIX64
-                 " %" PRIX64 " %" PRIX64  " %" PRIX64 " seed=%d\n",
-                 bui1_tr.csum, bui2_tr.csum, tdr1_tr.csum, tdr2_tr.csum,
-                 tdq1_tr.csum, tdi1_tr.csum, tdi2_tr.csum, seed);
-          exit(1);
-        }
-      }
-
-      tot_bui1_time /= NUM_SEEDS;
-      tot_bui2_time /= NUM_SEEDS;
-      tot_tdr1_time /= NUM_SEEDS;
-      tot_tdr2_time /= NUM_SEEDS;
-      tot_tdq1_time /= NUM_SEEDS;
-      tot_tdi1_time /= NUM_SEEDS;
-      tot_tdi2_time /= NUM_SEEDS;
-
-      printf(",%g,%g,%g,%g,%g,%g,%g\n",
-             tot_bui1_time, tot_bui2_time,
-             tot_tdr1_time, tot_tdr2_time, tot_tdq1_time,
-             tot_tdi1_time, tot_tdi2_time);
+      run_int64_list_tests(buf, tr, tot_time, elems, 1, NUM_SEEDS);
     }
   }
   
